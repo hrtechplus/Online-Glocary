34a8901da4818b0e6a7985bd7f33768d774c5dfb
hrtechplus <ranjithgunasekara9@gmail.com>
2023-10-13 19:01:42 +0530

    added

---
+++ src/main/java/os/project/connectionProvider.java
@@ -0,0 +1,17 @@
+package os.project;
+import java.sql.*;
+public class connectionProvider {
+	public static Connection getcon(){
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/school?autoReconnect=true&useSSL=false","root","root");
+			return conn;
+			
+		}
+		catch(Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+
+}
